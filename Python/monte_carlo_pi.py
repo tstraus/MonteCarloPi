5d86a410cbe82a8571d3463fc60c01d351df27f8
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2018-03-28 19:05:55 -0400

    'argv'

--- Python/monte_carlo_pi.py
+++ Python/monte_carlo_pi.py
@@ -1,22 +1,26 @@
 import random
+from sys import argv
 
-def InUnitCircle (x: float, y: float):
+
+def inUnitCircle (x: float, y: float):
     if x*x + y*y < 1.0:
         return True
     else:
         return False
 
-def MonteCarloPi (reps: int):
+
+def monteCarloPi (reps: int):
     count = 0
 
     for rep in range(reps):
-        if InUnitCircle(random.uniform(0.0, 1.0), random.uniform(0.0, 1.0)):
+        if inUnitCircle(random.uniform(0.0, 1.0), random.uniform(0.0, 1.0)):
             count += 1
 
     return count
 
-reps = 1000000
-count = MonteCarloPi(reps)
+
+reps = int(argv[1])
+count = monteCarloPi(reps)
 pi = count / reps * 4
 
 print("pi: " + str(pi))
